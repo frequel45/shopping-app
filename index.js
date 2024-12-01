74bcf0afc658412cd376bfeada2043ef8d5c1fb4
frequel45 <154789727+frequel45@users.noreply.github.com>
2024-12-01 11:40:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ if ("serviceWorker" in navigator) {
         .register("/service-worker.js")
         .then(() => console.log("Service Worker Registered"));
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the default mini-infobar prompt
+    e.preventDefault();
+    // Save the event for later use
+    deferredPrompt = e;
+
+    // Show your custom install button
+    const installButton = document.getElementById("install-button");
+    installButton.style.display = "block";
+
+    // Add a click event listener to the install button
+    installButton.addEventListener("click", () => {
+        // Show the install prompt
+        deferredPrompt.prompt();
+
+        // Wait for the user's choice
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt");
+            } else {
+                console.log("User dismissed the A2HS prompt");
+            }
+            // Reset the deferredPrompt variable
+            deferredPrompt = null;
+        });
+    });
+});
